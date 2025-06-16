e6caeb05a74d0a0362a4ddfe06f06c39dbe5248d
zabronax <zabronax@gmail.com>
2025-06-16 13:15:23 +0200

    setup example handlers

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
 var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 
+app.MapGet("/hello", () => {
+    return "Hello World";
+});
+
+app.MapGet("/bye", () => {
+    return "Goodbye!";
+});
+
 app.Run();
